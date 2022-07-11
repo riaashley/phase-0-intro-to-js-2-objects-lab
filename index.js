aae73200fe5490807f59f817902ff8312463aa1b
Ria Legaspi <riaveluz@gmail.com>
2022-07-10 20:22:09 -0700

    commit tree 5d4d60283962f5415693422ef8d93a33c5d9ad9b

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Sam',
+    streetAddress: '11 Broadway',
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newUpdate = {...employee};
+    newUpdate[key] = value;
+    return newUpdate;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
